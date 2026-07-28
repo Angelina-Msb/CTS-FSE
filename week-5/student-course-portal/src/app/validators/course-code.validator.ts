43bbf44aa8f0042aa8c83bfbb70164009d7293af
Angelina-Msb <munipalliangelina@gmail.com>
2026-07-28 16:40:40 +0530

    Delete week-5/student-course-portal directory

--- week-5/student-course-portal/src/app/validators/course-code.validator.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-
-export function courseCodeValidator(control: AbstractControl): ValidationErrors | null {
-  const value = control.value;
-  if (value && !value.toUpperCase().startsWith('CS')) {
-    return { invalidCourseCode: true };
-  }
-  return null;
-}
