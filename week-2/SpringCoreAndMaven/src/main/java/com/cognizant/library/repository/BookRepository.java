7bba47c7f54910778ff586f1fad79524b8d7e1b5
Angelina-Msb <munipalliangelina@gmail.com>
2026-07-28 16:40:12 +0530

    Delete week-2 directory

--- week-2/SpringCoreAndMaven/src/main/java/com/cognizant/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.library.repository;
-
-public class BookRepository {
-    
-    public void saveBook(String bookTitle) {
-        System.out.println("Repository: Successfully saved '" + bookTitle + "' to the database.");
-    }
-}
